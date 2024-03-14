7b2fd738a744bdce4d6687e11ee179d1e60782d5
jeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2024-03-14 12:15:10 +0900

    Title: 특정 위치의 문자, Time: 142ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240314/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+        
+        char[] word = new char[]{'L','E','R','O','S'};
+        char a = scanner.next().charAt(0);
+
+        int idx = -1;
+
+        for(int i=0; i<5; i++){
+            if(word[i]==a){
+                idx = i;
+            }
+        }
+
+        if(idx == -1){
+            System.out.print("None");
+        } else {
+            System.out.print(idx);
+        }
+    }
+}
