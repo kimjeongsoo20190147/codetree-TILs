105724bb0358272804de1f7990cedbaad3a876f3
jeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2024-07-30 21:43:33 +0900

    Title: 1이 3개 이상 있는 위치, Time: 142ms, Memory: 29MB, Status: Passed - CodeTree

--- 240730/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240730/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -13,15 +13,15 @@ def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 
-for x in range(0,n-1):
-    for y in range(0,n-1):
+for x in range(n):
+    for y in range(n):
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
             if in_range(nx,ny) and arr[nx][ny] == 1:
                 cnt += 1
         if cnt >= 3:
             ans += 1
-    cnt = 0
+        cnt = 0
             
 
 print(ans)
