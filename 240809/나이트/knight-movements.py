b847b7b04bfc1e8f3434056d5eb60b3f2c297705
jeongsoo <102466913+kimjeongsoo20190147@users.noreply.github.com>
2024-08-09 13:16:22 +0900

    Title: 나이트, Time: 148ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240809/나이트/knight-movements.py
+++ 240809/나이트/knight-movements.py
@@ -1,15 +1,44 @@
-n = int(input())
+from collections import deque
 
-start_x, start_y, end_x, end_y = map(int, input().split())
 
-visited = [[False] * n for _ in range(n)]
-print(visited)
+n = int(input().strip())
+start_x, start_y, end_x, end_y = map(int, input().strip().split())
 
-def in_range(x,y):
+dxs = [-2, -1, 1, 2, 2, 1, -1, -2]
+dys = [-1, -2, -2, -1, 1, 2, 2, 1]
+
+def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-
-
-def bfs(x,y):
-    if x == y :
+def bfs_knight(n, start, end):
+    if start == end:
         return 0
+    
+    
+    visited = [[False] * n for _ in range(n)]
+    queue = deque([(start[0], start[1], 0)])
+    visited[start[0]][start[1]] = True
+    
+    while queue:
+        x, y, dist = queue.popleft()
+        print(x,y,dist)
+        
+        
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            
+            if in_range(nx, ny) and not visited[nx][ny]:
+                if (nx, ny) == (end[0], end[1]):
+                    return dist + 1
+                queue.append((nx, ny, dist + 1))
+                visited[nx][ny] = True
+    
+    return -1
+
+
+
+
+start = (start_x - 1, start_y - 1)
+end = (end_x - 1, end_y - 1)
+
+print(bfs_knight(n, start, end))
